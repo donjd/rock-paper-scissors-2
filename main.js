38a3ace9b6cee25b153a9b8bf28fc44e26a45333
donjd <99505859+donjd@users.noreply.github.com>
2025-05-06 21:29:47 -0600

    Add generateComputerChoice function

--- main.js
+++ main.js
@@ -7,27 +7,33 @@ const computerChoiceDisplay = document.querySelector("#computer-choice");
 
 let userChoice = "";
 let computerChoice = "";
-let randomNumber = 0;
 
 options.addEventListener("click", generateUserChoice)
 
 function generateUserChoice (e){
     userChoice = e.target.id;
     if (userChoice == ""){
+        return;
     }
     else {
     userChoiceDisplay.textContent = userChoice;
+    generateComputerChoice();
     }
-    
 }
 
-//user choice is blank
-//computer choice is blank
-//random number is 0
-//user clicks on option
-//user choice is set to the id of the option
-//random number between 1 and 3 is generated
-//if number is 1, rock, if 2, paper, is 3, scissors
-//computer choice is set to corresponding weapon
-//change text content of #user-choice to userChoice
-//change text content of #computer-choice to computerChoice
+function generateComputerChoice() {
+    let randomNumber = Math.ceil(Math.random()*3);
+
+    switch (randomNumber){
+        case 1:
+            computerChoiceDisplay.textContent = "rock";
+            break;
+        case 2:
+            computerChoiceDisplay.textContent = "paper";
+            break;
+        default:
+            computerChoiceDisplay.textContent = "scissors";
+            break;
+    }
+
+}
